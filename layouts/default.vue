5220b61c4a41853be83eb06e05806d9c843db46d
Trystan <loke1118@hotmail.com>
2024-06-10 14:17:58 -0400

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+			<!-- <nav>
+					<ul>
+							<li>
+									<NuxtLink to="/">Home</NuxtLink>
+							</li>
+							<li>
+									<NuxtLink to="/about">About</NuxtLink>
+							</li>
+					</ul>
+			</nav> -->
+			<UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+			<slot />
+			<AppFooter />
+	</div>
+</template>
+<script setup lang="ts">
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'About',
+  icon: 'information-circle',
+  to: `/about`
+}]
+</script>
